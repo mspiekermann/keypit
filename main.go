e5980bc6f488fb5bc5da9f2232f34619ba7b8ed3
mspiekermann <m-spiekermann@web.de>
2026-07-13 10:01:55 +0000

    add getAll route

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
+type App struct {
+	store  Store
+	logger *log.Logger
+}
+
 func main() {
 
-	mux := http.NewServeMux()
+	app := App{
+		store:  *NewStore(),
+		logger: log.Default(),
+	}
 
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello from Keyp it!")
-	})
+	mux := app.routes()
 
 	server := &http.Server{
 		Handler: mux,
@@ -20,4 +28,49 @@ func main() {
 
 	server.ListenAndServe()
 
+}
+
+func (app *App) routes() http.Handler {
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello from Keyp it!")
+	})
+
+	mux.HandleFunc("PUT /add", app.handleAddKeyValuePair)
+	mux.HandleFunc("GET /all", app.handleGetAll)
+
+	return mux
+}
+
+func (app *App) handleAddKeyValuePair(w http.ResponseWriter, r *http.Request) {
+
+	type AddKeyValuePairRequest struct {
+		Key   string `json:"key"`
+		Value any    `json:"value"`
+	}
+
+	var data AddKeyValuePairRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		log.Printf("decoding request body: %v", err)
+		http.Error(w, "invalid JSON request body", http.StatusBadRequest)
+		return
+	}
+
+	app.store.Add(data.Key, data.Value)
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func (app *App) handleGetAll(w http.ResponseWriter, r *http.Request) {
+
+	data := app.store.GetAll()
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		log.Printf("encoding response: %v", err)
+	}
 }
--- store.go
+++ store.go
@@ -9,3 +9,11 @@ func NewStore() *Store {
 	store.data = make(map[string]any)
 	return &store
 }
+
+func (s *Store) Add(key string, value any) {
+	s.data[key] = value
+}
+
+func (s *Store) GetAll() map[string]any {
+	return s.data
+}
